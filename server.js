ff066f78b8656b7e19fa0770df82f3fc0f03c99c
Park Dongseok <76927397+pds0309@users.noreply.github.com>
2022-12-13 02:07:17 +0900

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,16 @@ const {
   validPassword,
   sortedBy,
 } = require("./utils");
+const fs = require("fs");
+const os = require("os");
+
+fs.copyFile("db.json", os.tmpdir() + "/db.json", function (err) {
+  if (err) console.log(err);
+  else console.log("copy file succeed to" + os.tmpdir());
+});
 
 const server = jsonServer.create();
-const router = jsonServer.router("http://hang99-10-e-dai.com/images/db.json");
+const router = jsonServer.router(path.resolve(os.tmpdir() + "/db.json"));
 const middlewares = jsonServer.defaults();
 
 
